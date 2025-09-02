6e188b37cfbe927eb68167896559b0b81b1e6ba8
Dragon <luismanco7@gmail.com>
2025-09-02 15:40:10 -0500

    DataContext

---
+++ ECommerce.Web/Models/ECommerceContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Web.Models
+{
+    public class ECommerceContext :DbContext
+    {
+        public ECommerceContext() : base("DefaultConnection")
+        {
+            
+        }
+    }
+}
